6fdd09a64132d9a29605abb2fc3cb48e0f406a57
brunoand <bgabriel.andrade@gmail.com>
2018-05-08 16:30:53 -0300

    new

--- Scripts/merge_taxonomy.py
+++ Scripts/merge_taxonomy.py
@@ -11,6 +11,7 @@ def replace_all(text, dic):
 
 Input_OTU = sys.argv[1]
 Taxonomy = sys.argv[2]
+Output = sys.argv[3]
 
 OTU_table = pd.read_csv(Input_OTU, sep = '\t')
 header_taxonomy = ['OTU', 'Taxonomy', 'Score', 'Size']
@@ -18,5 +19,4 @@ Taxonomy = pd.read_csv(Taxonomy, sep = '\t', names = header_taxonomy)
 Pattern = {"D_0":"d", "D_1":"p", "D_2":"c", "D_3":"o", "D_4":"f", "D_5":"g", "D_6":"s" }
 
 Taxonomy['Taxonomy']= replace_all(Taxonomy['Taxonomy'], Pattern)
-#print(OTU_table)
-Taxonomy.merge(OTU_table, right_on='OTU', left_on = 'OTU').drop(['Size'], axis=1).set_index('OTU').to_csv('OTU_table_taxonomy.txt', sep = '\t')
+Taxonomy.merge(OTU_table, right_on='OTU', left_on = 'OTU').drop(['Size'], axis=1).set_index('OTU').to_csv(Output, sep = '\t')
