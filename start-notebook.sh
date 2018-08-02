ce753b4a7bda524c94f39bc4e7be0f764bdc66b0
brunoand <30294487+brunoand@users.noreply.github.com>
2018-08-01 23:47:03 -0300

    Update start-notebook.sh

--- start-notebook.sh
+++ start-notebook.sh
@@ -4,13 +4,13 @@
 if [ $UID == 0 ] ; then 
  # Change UID of NB_USER to NB_UID if it does not match 
  if [ "$NB_UID" != $(id -u $NB_USER) ] ; then 
- usermod -u $NB_UID $NB_USER 
- chown -R $NB_UID $CONDA_DIR . 
+  usermod -u $NB_UID $NB_USER 
+  chown -R $NB_UID $CONDA_DIR . 
  fi 
  
  # Enable sudo if requested 
  if [ ! -z "$GRANT_SUDO" ]; then 
- echo "$NB_USER ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/notebook 
+  echo "$NB_USER ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/notebook 
  fi 
  
  # Start the notebook server 
